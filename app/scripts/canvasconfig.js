9a2c29585d2f5f2d9d9d17274f948097ab887ee0
Jack Schuss <jackschuss@gmail.com>
2016-08-28 14:27:01 -0400

    add paddle constructor, table line

--- app/scripts/canvasconfig.js
+++ app/scripts/canvasconfig.js
@@ -1,5 +1,45 @@
 var CanvasConfig = (function () {
+  var Constructor = {};
 
+  var canvas = document.getElementById('pong-board');
+  var context = canvas.getContext('2d');
+
+  var drawPingPongLine = function () {
+    context.beginPath();
+
+    context.moveTo(canvas.width/2, 0);
+    context.lineTo(canvas.width/2, canvas.height);
+    context.lineWidth = 5;
+
+    context.strokeStyle = '#fff';
+
+    context.stroke();
+  
+  };
+
+  Constructor.Paddle = function (x, y){
+    this.x = x;
+    this.y = y;
+
+    context.beginPath();
+
+    context.moveTo(x, y);
+    context.lineTo(x, y + 200);
+
+    context.lineWidth = 20;
+    context.strokeStyle = '#fff';
+    context.lineCap = 'round';
+
+    context.stroke();
+
+  }
+
+  var rightPaddle = new Constructor.Paddle(30, 90);
+  var leftPaddle = new Constructor.Paddle(710, 240);
+  drawPingPongLine();
+
+
+  return Constructor;
 })();
 
 
