aef12c4a682b81604ec79a127c1787c294bd320b
Jack Schuss <jackschuss@gmail.com>
2016-08-29 10:56:12 -0400

    move drawpingpong to intial rendering func

--- app/scripts/initialRendering.js
+++ app/scripts/initialRendering.js
@@ -1,12 +1,14 @@
 var Ball = require('./Ball');
 var Computer = require('./Computer');
 var Player = require('./Player');
+var CanvasConfig = require('./canvasconfig');
 
 var initialRendering = function () {
 
 computerPaddle = new Computer();
 playerPaddle = new Player();
 ball = new Ball();
+CanvasConfig.drawPingPongLine();
 
 }
 
